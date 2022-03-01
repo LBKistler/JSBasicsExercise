feec2300af4acbd1911ab562fc5e8603b87fd32f
“lbkistler” <loribethkistler@gmail.com>
2022-02-28 20:48:09 -0600

    uploaded JSBasicsExercise

---
+++ jsBasicsExercise.js
@@ -0,0 +1,57 @@
+// 1
+console.log(457);
+
+// 2
+// The 5 main types of PRIMITIVES are Numbers, Strings, Booleans, Null, and Undefined.
+
+// 3
+/* The 6 OPERATOR SYMBOLS are
+Adding ( + )
+Subtracting ( - )
+Multiplying ( * )
+Dividing ( / )
+Exponentiation ( ** )
+Modulus ( % )
+*/
+
+// 4
+console.log(5 % 4);
+console.log(6 % 5);
+
+// 5
+console.log("I'M MARY POPPINS, Y'ALL!");
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("LB".length + "Kistler".length);
+
+// 8
+console.log("Avengers:Endgame"[15]);
+
+// B1
+console.log("Inevitable/"[10]);
+
+// B2
+console.log((5 ** 4) - 9);
+
+// B3
+console.log(("I".length - "Am".length) + ("Iron".length - "Man".length));
+console.log(("I".length - "Am".length) + ("Iron".length) % "Man".length);
+
+// B4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// B5
+m = 25;
+c = 4;
+u = m + c;
+console.log(u);
